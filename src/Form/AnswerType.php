1cfbcf07a159c2230971ff049bf83f32d8f46377
Arnaud-Nova <calmelsarnaud@gmail.com>
2019-03-07 19:20:38 +0100

    crud Answer et Tag

---
+++ src/Form/AnswerType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Answer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnswerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('content')
+            ->add('isActive')
+            ->add('createdAt')
+            ->add('question')
+            ->add('valideForQuestion')
+            ->add('author')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Answer::class,
+        ]);
+    }
+}
