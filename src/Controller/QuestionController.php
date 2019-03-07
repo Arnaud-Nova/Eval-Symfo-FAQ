fffb7439a908863b1a0460745b0f37d479a7f775
Arnaud-Nova <calmelsarnaud@gmail.com>
2019-03-07 18:19:45 +0100

    page question avec ses réponses

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\QuestionRepository;
+use App\Entity\Question;
 
 class QuestionController extends AbstractController
 {
@@ -17,6 +18,21 @@ class QuestionController extends AbstractController
 
         return $this->render('question/index.html.twig', [
             'questions' => $questions,
+        ]);
+    }
+
+    /**
+     * @Route("/question/{id}", name="show_question", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    public function show(Question $question)
+    {
+        // dd($question);
+        if (!$question) {
+            throw $this->createNotFoundException('Cette question est introuvable');
+        }
+
+        return $this->render('question/show.html.twig', [
+            'question' => $question,
         ]);
     }
 }
